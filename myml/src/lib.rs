7836dc6bc86f40157b9b7e21d6e91ff56f357afc
Nine-rem <alexis2000.te@hotmail.fr>
2025-08-29 15:49:20 +0200

    cleaned dataset

--- myml/src/lib.rs
+++ myml/src/lib.rs
@@ -1,14 +1,13 @@
-﻿use pyo3::prelude::*;
+﻿// src/lib.rs
+use pyo3::prelude::*;
 
 pub mod activation;
 pub mod matrix;
 pub mod network;
 
 #[pymodule]
-fn myml_core(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
-
+fn myml(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<network::Network>()?;
-
     m.add("SIGMOID", activation::ActivationKind::Sigmoid as u8)?;
     m.add("TANH",    activation::ActivationKind::Tanh    as u8)?;
     m.add("RELU",    activation::ActivationKind::Relu    as u8)?;
